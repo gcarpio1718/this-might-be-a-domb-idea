ea4b71ba3129afea61717bc57eb650330f661945
gcarpio1718 <gcarpio1221@gmail.com>
2022-03-14 12:35:26 -0400

    change main

---
+++ main.js
@@ -0,0 +1,26 @@
+let paragraph = document.querySelector('p');
+paragraph.style.color = 'lightblue';
+
+let heading = document.querySelector('h1');
+heading.style.fontSize = '48px';
+
+paragraph.innerText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi consequatur necessitatibus quod nesciunt esse, corporis aliquam ullam deleniti dolore neque autem! Nihil tempora impedit earum perferendis pariatur molestiae sequi possimus?";
+
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = '.5';
+
+let item3 = document.querySelector('#item-3');
+item3.innerText = 'I say, "Hi!"';
+
+let image = document.querySelector('img');
+image.src = "http://www.tioxic.com/wp-content/uploads/trex_4.jpg";
+
+image.style.height = "300px";
+
+let album = document.querySelector('#net')
+net.style.height ='300px';
+
+let item16 = document.querySelector('#item-16');
+item16.className = 'item';
+item16.innerText = "Won't get fooled again."; 
+
